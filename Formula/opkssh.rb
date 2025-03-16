43ee574b12c48c3d1812337213a8937fdb4acda8
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-16 17:11:53 -0400

    Ensures installed opkssh has version information

--- Formula/opkssh.rb
+++ Formula/opkssh.rb
@@ -1,13 +1,15 @@
 class Opkssh < Formula
     desc "opkssh (OpenPubKey SSH) - SSH with OpenID Connect"
     homepage "https://github.com/openpubkey/opkssh"
-    url "https://github.com/openpubkey/opkssh/archive/refs/tags/v0.2.1.tar.gz"
+    version "0.2.1" # Replace with the latest version
+
+    url "https://github.com/openpubkey/opkssh/archive/refs/tags/v#{version}.tar.gz"
     sha256 "e36c49480a08d05d01bc3534f1e31a76947a5f1acb96e48139e745d30c13c8de"
     license "APACHE-2.0"
     depends_on "go@1.23" => :build
   
     def install
-      system "go", "build", "-o", bin/"opkssh"
+      system "go", "-ldflags", "-X main.Version=#{version}", "build", "-o", bin/"opkssh"
     end
   
     test do
