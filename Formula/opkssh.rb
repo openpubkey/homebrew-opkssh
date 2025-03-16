358f129d8a27743ef58da984575a2916dd736fe4
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-16 16:42:20 -0400

    Fixes source code path after change to opkssh repo

--- Formula/opkssh.rb
+++ Formula/opkssh.rb
@@ -1,14 +1,13 @@
 class Opkssh < Formula
     desc "opkssh (OpenPubKey SSH) - SSH with OpenID Connect"
     homepage "https://github.com/openpubkey/opkssh"
-    url "https://github.com/openpubkey/opkssh/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "b0915465633f6a14244fafac5d0d8a74eec5ebf40f9c2eeb542f2d8f510dc72f"
+    url "https://github.com/openpubkey/opkssh/archive/refs/tags/v0.2.1.tar.gz"
+    sha256 "e36c49480a08d05d01bc3534f1e31a76947a5f1acb96e48139e745d30c13c8de"
     license "APACHE-2.0"
-  
     depends_on "go@1.23" => :build
   
     def install
-      system "go", "build", "-o", bin/"opkssh", "./opkssh"
+      system "go", "build", "-o", bin/"opkssh"
     end
   
     test do
