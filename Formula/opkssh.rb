d279b20703ae5ec0a2d8909b8593588daf3ab011
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-21 17:35:41 -0400

    Disables bottle

--- Formula/opkssh.rb
+++ Formula/opkssh.rb
@@ -8,10 +8,11 @@ class Opkssh < Formula
     sha256 "fa5ac8a956837c9f2959d08720dbf31b3713ebafdf8b47ec9408e8a16605d080"
     license "APACHE-2.0"
 
-    bottle do
-      root_url "https://github.com/openpubkey/opkssh/releases/download/v#{opkssh_version}"
-      sha256 cellar: :any_skip_relocation, all: "1e642eccd1125aa7b76465fef2aa4283b1a853e4843c9d79dbce0fa60d25badc"
-    end
+    # Bottle disabled for now 
+    # bottle do
+    #   root_url "https://github.com/openpubkey/opkssh/releases/download/v#{opkssh_version}"
+    #   sha256 cellar: :any_skip_relocation, all: "1e642eccd1125aa7b76465fef2aa4283b1a853e4843c9d79dbce0fa60d25badc"
+    # end
 
     depends_on "go@1.23" => :build
   
