4a7fa961322d6c2e6a69806e05067bd246db8b3c
Ethan Heilman <ethan.r.heilman@gmail.com>
2025-03-21 17:26:19 -0400

    Adds ability to install from bottle

--- Formula/opkssh.rb
+++ Formula/opkssh.rb
@@ -1,11 +1,18 @@
 class Opkssh < Formula
     desc "opkssh (OpenPubKey SSH) - SSH with OpenID Connect"
     homepage "https://github.com/openpubkey/opkssh"
-    version "0.2.2" # Replace with the latest version
+    opkssh_version = "0.2.2" # Replace with the latest version
+    version opkssh_version
 
     url "https://github.com/openpubkey/opkssh/archive/refs/tags/v#{version}.tar.gz"
     sha256 "fa5ac8a956837c9f2959d08720dbf31b3713ebafdf8b47ec9408e8a16605d080"
     license "APACHE-2.0"
+
+    bottle do
+      root_url "https://github.com/openpubkey/opkssh/releases/download/v#{opkssh_version}"
+      sha256 cellar: :any_skip_relocation, all: "1e642eccd1125aa7b76465fef2aa4283b1a853e4843c9d79dbce0fa60d25badc"
+    end
+
     depends_on "go@1.23" => :build
   
     def install
